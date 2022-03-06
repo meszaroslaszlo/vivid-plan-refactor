1bbd987b35188d5c8628d461973f9c53c439af5f
László Mészáros <meszaros.laszlo@nexius.hu>
2022-03-06 19:20:19 +0100

    Init

---
+++ depgraph.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  projectDirectory: "C:/Users/andro/dev/vivid-plan-refactor",
+  tsconfig: "C:/Users/andro/dev/vivid-plan-refactor/tsconfig.json",
+  includePattern: ".ts$",
+  excludePattern: ".spec.ts$",
+};
